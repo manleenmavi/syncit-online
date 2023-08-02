8e1102c6c6f1141d533d16153f5293689ab09da8
manleenmavi <manleenmavi@gmail.com>
2023-08-01 23:09:03 -0600

    Initial commit. Vite, Vue, and tailwind config.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import LetsSync from './components/LetsSync.vue'
+</script>
+
+<template>
+  <div class="min-h-screen w-full bg-color-primary">
+    <LetsSync />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/LetsSync.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center items-center w-full min-h-screen">
+    <img src="../assets/syncit_logo-full.png" alt="logo" class="w-1/2" />
+  </div>
+</template>
+
+<style scoped></style>
